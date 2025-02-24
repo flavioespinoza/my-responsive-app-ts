91c09127726a6c76a1f475ff5e51c0051b1b9d48
Flavio Espinoza <flavio.espinoza@gmail.com>
2025-02-23 22:20:07 -0700

    center

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en" suppressHydrationWarning>
       <body className="flex items-center justify-center min-h-screen bg-background text-foreground">
         <ThemeProvider defaultTheme="light" enableSystem={true}>
-          <main className="w-full p-4 max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl rounded-lg shadow-lg flex flex-col items-center bg-background dark:bg-background text-foreground">
+          <main id="app_container" className="w-full p-4 max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl rounded-lg shadow-lg flex flex-col items-center bg-background dark:bg-background text-foreground">
             {children}
           </main>
         </ThemeProvider>
